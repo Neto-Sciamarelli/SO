3fbc19638c006450496d1be9873e403ccffb762e
Henrique Árabe Neres de Farias <118843050+HenriqueArabe@users.noreply.github.com>
2024-03-11 13:26:41 -0300

    Update Pai_Filho.c

--- Lab2/Pai_Filho.c
+++ Lab2/Pai_Filho.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h> 
 
 int main() {
     int pipe_pai_filho[2];
@@ -24,32 +25,34 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    if (pid == 0) {
-        // Código do processo filho
-        close(pipe_pai_filho[1]); 
+    if (pid == 0) { // Código do processo filho
+        close(pipe_pai_filho[1]);  
         char msg_filho[60]; 
         read(pipe_pai_filho[0], msg_filho, sizeof(msg_filho)); 
         printf("Filho recebeu: %s\n", msg_filho);
 
-        // Responde ao pai
-        close(pipe_pai_filho[0]); 
-        close(pipe_filho_pai[0]); 
+        // Prepara e envia a resposta ao pai
+        close(pipe_pai_filho[0]);  
+        close(pipe_filho_pai[0]);  
         const char* resposta_filho = "Eae, pai. Estou bem e o senhor?";
-        write(pipe_filho_pai[1], resposta_filho, 35); 
-        close(pipe_filho_pai[1]); 
-    } else {
-        // Código do processo pai
-        close(pipe_pai_filho[0]);
+        write(pipe_filho_pai[1], resposta_filho, strlen(resposta_filho) + 1); 
+        close(pipe_filho_pai[1]);  
+        
+    } else { // Código do processo pai
+        close(pipe_pai_filho[0]);  
         const char* msg_pai = "Olá, filho. Como você está?";
-        write(pipe_pai_filho[1], msg_pai, 30); 
-        close(pipe_pai_filho[1]); 
+        write(pipe_pai_filho[1], msg_pai, strlen(msg_pai) + 1);
+        close(pipe_pai_filho[1]);  
 
-        // Espera pela resposta do filho
+        // Espera pelo processo filho terminar
+        wait(NULL);
+
+        // Prepara-se para receber a resposta do filho
         close(pipe_filho_pai[1]); 
         char resposta_pai[60]; 
-        read(pipe_filho_pai[0], resposta_pai, sizeof(resposta_pai));
+        read(pipe_filho_pai[0], resposta_pai, sizeof(resposta_pai)); 
         printf("Pai recebeu: %s\n", resposta_pai);
-        close(pipe_filho_pai[0]); 
+        close(pipe_filho_pai[0]);  
     }
 
     return 0;
